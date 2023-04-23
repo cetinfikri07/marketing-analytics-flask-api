5dc28eb2c29a7e023a38addaeb157a0448150ae0
cetinfikri07 <fikri.cetin07@gmail.com>
2023-04-23 23:05:37 +0300

    none

--- insights-api/reporting_api/__init__.py
+++ insights-api/reporting_api/__init__.py
@@ -2,12 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 from flask_swagger_ui import get_swaggerui_blueprint
 
-from reporting_api.db.models import Users
-from reporting_api.db import session
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5b69f5016ac09b6197e9e95b16c78549896f413905fe3e8477a529390495f588'
-
 CORS(app)
 
 #Register blueprints
